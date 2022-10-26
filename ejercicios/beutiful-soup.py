1855468492019d0be0c7f93aa1d67a8fabbdb731
Alex900930 <aherreramilet@gmail.com>
2022-10-25 20:37:40 -0500

    selenium version1

--- ejercicios/beutiful-soup.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-
-
-r = requests.get('https://www.realtor.com/realestateandhomes-search/Florida')
-soup = BeautifulSoup(r.text, 'lxml')
-
-print(soup)
